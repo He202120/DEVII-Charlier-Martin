138c174a84b2358be454872dc29ed85da4c8b223
Martin <charliermart@outlook.fr>
2023-12-18 16:40:51 +0100

    Ajout de fonctions tel que renommer,supprimer,regrouper. Plus, ajout des décorateurs click pour ces différentes fonctions

--- Script-Tri.py
+++ Script-Tri.py
@@ -7,8 +7,11 @@ import click
 @click.argument('source', type=click.Path(), metavar='<dossier_source>')
 @click.argument('destination', type=click.Path(), metavar='<dossier_destination>')
 @click.option('--afficher-contenu', is_flag=True, help='Afficher le contenu du dossier source avant le tri.')
+@click.option('--tri-par-extension', is_flag=True, help='Trier les fichiers par extension dans des sous-dossiers.')
+@click.option('--renommer-fichiers', is_flag=True, help='Renommer les fichiers avec un nombre devant (nombre croissant)')
+@click.option('--supprimer-fichiers', is_flag=True, help='Supprimer les fichiers du dossier source après le tri.')
 @click.option('--tri-regroupement', is_flag=True, help='Regroupe les fichiers en fonction de leurs extension')
-def trier_fichiers(source, destination, afficher_contenu,tri_regroupement):
+def trier_fichiers(source, destination, afficher_contenu, tri_par_extension, renommer_fichiers, supprimer_fichiers, tri_regroupement):
     """
     Script pour trier les fichiers d'un dossier source vers un dossier destination.
 
@@ -18,15 +21,53 @@ def trier_fichiers(source, destination, afficher_contenu,tri_regroupement):
     """
 
     if not os.path.exists(source):
-        raise click.ClickException("Le fichier1 source n'existe pas")
+        raise click.ClickException(f"Le fichier source : {source} ,n'existe pas")
 
     if afficher_contenu:
         click.echo(f"Contenu du dossier source ({source}):")
         for fichier in os.listdir(source):
             click.echo(f"- {fichier}")
+    elif tri_par_extension:
+        trier_par_extension(source, destination)
+    elif renommer_fichiers:
+        renommer_avec_prefixe_numerique(source)
+    elif supprimer_fichiers:
+        supprimer_fichiers_source(source)
     elif tri_regroupement:
         trier_regroupement(source, destination)
+    else:
+        click.ClickException("la commande renseignée n'existe pas")
 
+
+def trier_par_extension(source, destination):
+    for fichier in os.listdir(source):  # Va passer sur chaque fichier1
+        chemin_source = os.path.join(source, fichier)  # Chemin absolu du fichier1 source
+        if os.path.isfile(chemin_source):   # Vérifie si c'est un fichier1
+            nom_fichier, extension = os.path.splitext(fichier)  # Divise le nom et l'extension du fichier1 source
+            if extension != "":  # Si pas d'extension alors pas de tri effectué et reste dans le fichier1 source
+                dossier_destination = os.path.join(destination, extension[1:])  # Va creer le repértoire de destination si il n'existe pas et dans un fichier1 au nom de l'extension ([1:] pour pas prendre le .)
+                os.makedirs(dossier_destination, exist_ok=True)  # Permet de ne pas écraser si un fichier1 du même nom existe déja dans le répertoire distanation si la valuer du exist_ok est false alors voici l'erreur qui s'affiche en cas de doublon : FileExistsError: [WinError 183] Impossible de créer un fichier1 déjà existant: 'fichier2\\jpg'
+                shutil.move(os.path.join(source, fichier), os.path.join(dossier_destination, fichier))
+    click.echo("Fichiers triés par extension.")
+
+
+def renommer_avec_prefixe_numerique(source):
+
+    i = 1
+    for fichier in os.listdir(source):
+        if os.path.isfile(os.path.join(source, fichier)):
+            nouveau_nom = f"{i}_{fichier}"
+            os.rename(os.path.join(source, fichier), os.path.join(source, nouveau_nom))
+            i += 1
+    click.echo("Fichiers renommés avec préfixe numérique.")
+
+
+def supprimer_fichiers_source(source):
+    for fichier in os.listdir(source):
+        chemin_fichier = os.path.join(source, fichier)
+        if os.path.isfile(chemin_fichier):
+            os.remove(chemin_fichier)
+    click.echo("Fichiers supprimés du dossier source.")
 
 
 def trier_regroupement(source,destination):
